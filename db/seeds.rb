17c23fd9d759953eba46afad02c100118f49733a
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-04 14:04:19 -0500

    seeding added

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ require_relative '../config/application'
 require 'faker'
 
 
-list = List.create(:name => "BS LIST")
+List.create(:name => "BS LIST")
 
 10.times do
 Task.create(:description => Faker::Company.bs,
-             :list_id => list.select(:id) )
+             :list_id => List.select(:id).where(:name == "BS LIST") )
 end
 
 
