1b3ce693403f65f526c5dd93b752bd6a7ccd51d1
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-04 14:27:39 -0500

    modified tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 require_relative '../config/application'
 require 'faker'
+# require 'pry'
 
 
 List.create(:name => "BS LIST")
 
 10.times do
 Task.create(:description => Faker::Company.bs,
-             :list_id => List.select(:id).where(:name == "BS LIST") )
+             :list_id => List.select(:id).where(:name == "BS_LIST") )
+             # binding.pry
 end
 
 
+# Task.where(:id == 1).select(List.name)
