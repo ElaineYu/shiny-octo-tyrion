6c889550ae2af2cdfb0923a0af7763818e1fdf4f
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-04 12:23:00 -0500

    migrate files

---
+++ db/migrate/20131004120605_create_tasks.rb
@@ -0,0 +1,11 @@
+require_relative '../../config/application'
+
+class CreateTasks < ActiveRecord::Migration # "to create tasks"
+  def change
+    create_table :tasks do |t|
+      t.integer :list_id
+      t.string :description
+      t.timestamp
+    end
+  end
+end
